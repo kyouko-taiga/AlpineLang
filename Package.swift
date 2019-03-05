2675a6879629ad9a9973b460a8d15bf94fcfd870
Dimitri Racordon <dimitri.racordon@gmail.com>
2019-03-05 09:38:21 +0100

    Add a library product

--- Package.swift
+++ Package.swift
@@ -5,12 +5,14 @@ let package = Package(
   name: "AlpineLang",
   products: [
     .executable(name: "alpine", targets: ["alpine"]),
+    .library(name: "AlpineLib", targets: ["AlpineLib"]),
   ],
   dependencies: [
     .package(url: "https://github.com/kyouko-taiga/ArgParse.git", from: "1.1.0"),
   ],
   targets: [
-    .target(name: "alpine", dependencies: ["AST", "Interpreter", "Parser", "Sema", "ArgParse"]),
+    .target(name: "alpine", dependencies: ["AlpineLib", "ArgParse"]),
+    .target(name: "AlpineLib", dependencies: ["AST", "Interpreter", "Parser", "Sema"]),
     .target(name: "AST", dependencies: ["Utils"]),
     .target(name: "Interpreter", dependencies: ["AST", "Parser", "Sema"]),
     .target(name: "Parser", dependencies: ["AST"]),
